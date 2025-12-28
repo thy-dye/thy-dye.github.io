b56ebcd4eb2b733a2715eebf6cc9e6e570c0504c
Ty Torres <tiedye3.141@gmail.com>
2025-12-27 20:26:02 -0800

    getting the hang of webdev finally added some images and a shared css page also did two lectures for interactive graphics and CMUs intro graphics course expect repositories soon >:D

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import zanka_tired from '$lib/assets/zanka_tired.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={zanka_tired} />
 </svelte:head>
 
+<nav>
+	<a href="/">Ty Torres</a>
+	<a href="/Projects">PROJECTS</a>
+</nav>
+
+
 {@render children()}
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  
+  let cam: HTMLCanvasElement | null = null;
+  
+  onMount(() => {
+    // TypeScript knows canvas could be null, so we need to check
+    if (!cam) return;
+    
+    const ctx = cam.getContext('2d');
+    if (!ctx) return;
+    
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 350, 200);
+  });
+</script>
+
+<h1>Home</h1>
+<h3>Ty Torres</h3>
+<p>about me and blah blah</p>
+
+<canvas bind:this={cam} width="350" height="200"></canvas>
+<div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+
+<style>
+  canvas {
+    width: 350px;
+    height: 200px;
+    border: 2px solid black;
+  }
+</style>
